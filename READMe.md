aa1a801908bd5470d10aae2fddf85be685496674
rrc_f2024swagemakers <swagemakers@rrc.ca>
2024-08-30 08:49:24 -0500

    Added text that says my name and something I like

---
+++ READMe.md
@@ -0,0 +1,9 @@
+# Hello_World_Project
+ 
+## Description
+It displays the text "Hello World"
+It displays my name
+It displays my love of music
+ 
+## Author
+Sarah
---
+++ hello_world.py
@@ -0,0 +1,9 @@
+"""
+Description: Hello world display program
+Author: Sarah
+Date: August 30, 2024
+"""
+
+print("Hello World")
+print ("My name is Sarah!")
+print ("I like music")
