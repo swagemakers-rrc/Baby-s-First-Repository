e69743a827bbae5e1afbda60755fe3e32094e031
rrc_F2024_swagemakers <swagemakers@rrc.ca>
2024-09-08 19:05:08 -0500

    Added my corrected name to the hello.py file, edited the description of the assignment, struggled a lot.

--- READMe.md
+++ READMe.md
@@ -6,4 +6,8 @@ It displays my name
 It displays my love of music
  
 ## Author
-Sarah
+Sarah (Shelby) Wagemakers
+
+## Assignment
+Name: Assignment 1 (Module 1)
+Description: A small test to demonstrate our understanding of setting up a Git repo, making changes, adding descriptions, and pushing commits to our corresponding GitHub repo.
--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 """
-Description: Hello world display program
+Description: Hello World testing display program
 Author: Sarah Wagemakers
 Date: August 30, 2024
 """
@@ -7,3 +7,5 @@ Date: August 30, 2024
 print("Hello World")
 print ("My name is Sarah!")
 print ("I like music")
+
+More nonsense: is literally any of this being saved or pushed to github???
